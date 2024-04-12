31db8d534a5c1168716f548b0ea485c829e0ea76
apl26 <apl2612@mail.ru>
2024-04-12 18:08:00 +0300

    рефакторинг

--- ApplesGame/GameMain.cpp
+++ ApplesGame/GameMain.cpp
@@ -56,20 +56,56 @@ bool IsCircleCollide(Position2D circle1Position, float circle1Radius,
 
 }
 
+struct Player {
+	Position2D position;
+	float speed = INITIAL_SPEED;
+	PlayerDirection direction = PlayerDirection::Right;
+	sf::RectangleShape shape;
+};
+
+void InitPlayer(Player& player) {
+	player.position = { SCREEN_WIDTH / 2.f , SCREEN_HEIGHT / 2.f };
+	player.speed = INITIAL_SPEED;
+	player.direction = PlayerDirection::Right; //0 - Право, 1 - Верх, 2 - Лево, 3 - Низ
+
+	player.shape.setSize(sf::Vector2f(PLAYER_SIZE, PLAYER_SIZE));
+	player.shape.setFillColor(sf::Color::Blue);
+	player.shape.setOrigin(PLAYER_SIZE / 2.f, PLAYER_SIZE / 2.f);//центр игрока
+	player.shape.setPosition(player.position.x, player.position.y);
+}
+
+struct Apple {
+	Position2D position;
+	sf::CircleShape shape;
+};
+
+void InitApple(Apple& apple) {
+	apple.position = GetRandomPositionInScreen();
+
+	apple.shape.setRadius(APPLE_SIZE / 2.f);
+	apple.shape.setFillColor(sf::Color::Green);
+	apple.shape.setOrigin(APPLE_SIZE / 2.f, APPLE_SIZE / 2.f);//центр яблочка
+	apple.shape.setPosition(apple.position.x, apple.position.y);
+}
+
+struct Stone {
+	Position2D position;
+	sf::RectangleShape shape;
+};
+
+void InitStone(Stone& stone) {
+	stone.position = GetRandomPositionInScreen();
+
+	stone.shape.setSize(sf::Vector2f(STONE_SIZE, STONE_SIZE));
+	stone.shape.setFillColor(sf::Color::Red);
+	stone.shape.setOrigin(STONE_SIZE / 2.f, STONE_SIZE / 2.f);//центр яблочка
+	stone.shape.setPosition(stone.position.x, stone.position.y);
+}
+
 struct GameState {
-	// player data
-	Position2D playerPosition;
-	float playerSpeed = INITIAL_SPEED;
-	PlayerDirection playerDirection = PlayerDirection::Right;
-	sf::RectangleShape playerShape;
-
-	// apples data
-	Position2D applesPositions[NUM_APPLES];
-	sf::CircleShape applesShape[NUM_APPLES];
-
-	// stones data
-	Position2D stonePositions[NUM_STONES];
-	sf::RectangleShape stoneShape[NUM_STONES];
+	Player player;
+	Apple apples[NUM_APPLES];
+	Stone stones[NUM_STONES];
 
 	// glodal game data
 	int numEatenApples = 0;
@@ -78,46 +114,31 @@ struct GameState {
 	sf::RectangleShape background;
 };
 
-void InitGame(GameState& gameState) {
-	// расположение игрока
-	gameState.playerPosition = { SCREEN_WIDTH / 2.f , SCREEN_HEIGHT / 2.f };
-	gameState.playerSpeed = INITIAL_SPEED;
-	gameState.playerDirection = PlayerDirection::Right; //0 - Право, 1 - Верх, 2 - Лево, 3 - Низ
-
-	// форма игрока
-	sf::RectangleShape playerShape;
-	gameState.playerShape.setSize(sf::Vector2f(PLAYER_SIZE, PLAYER_SIZE));
-	gameState.playerShape.setFillColor(sf::Color::Blue);
-	gameState.playerShape.setOrigin(PLAYER_SIZE / 2.f, PLAYER_SIZE / 2.f);//центр игрока
-	gameState.playerShape.setPosition(gameState.playerPosition.x, gameState.playerPosition.y);
+void RestartGame(GameState& gameState) {
+	// игрок
+	InitPlayer(gameState.player);
 
 	// яблочки
 	for (int i = 0; i < NUM_APPLES; ++i) {
-		gameState.applesPositions[i] = GetRandomPositionInScreen();
-
-		gameState.applesShape[i].setRadius(APPLE_SIZE / 2.f);
-		gameState.applesShape[i].setFillColor(sf::Color::Green);
-		gameState.applesShape[i].setOrigin(APPLE_SIZE / 2.f, APPLE_SIZE / 2.f);//центр яблочка
-		gameState.applesShape[i].setPosition(gameState.applesPositions[i].x, gameState.applesPositions[i].y);
+		InitApple(gameState.apples[i]);
 	}
 
 	// камушки 
 	for (int i = 0; i < NUM_STONES; ++i) {
-		gameState.stonePositions[i] = GetRandomPositionInScreen();
-
-		gameState.stoneShape[i].setSize(sf::Vector2f(STONE_SIZE, STONE_SIZE));
-		gameState.stoneShape[i].setFillColor(sf::Color::Red);
-		gameState.stoneShape[i].setOrigin(STONE_SIZE / 2.f, STONE_SIZE / 2.f);//центр яблочка
-		gameState.stoneShape[i].setPosition(gameState.stonePositions[i].x, gameState.stonePositions[i].y);
+		InitStone(gameState.stones[i]);
 	}
 
 	gameState.numEatenApples = 0;
 	gameState.isGameFinished = false;
 	gameState.timeSinceGameFinish = 0;
+}
 
+void InitGame(GameState& gameState) {
 	gameState.background.setSize(sf::Vector2f(SCREEN_WIDTH, SCREEN_HEIGHT));
 	gameState.background.setFillColor(sf::Color::Black);
 	gameState.background.setPosition(0.f, 0.f);
+
+	RestartGame(gameState);
 }
 
 void UpdateGame(GameState& gameState, float deltaTime) {
@@ -125,63 +146,63 @@ void UpdateGame(GameState& gameState, float deltaTime) {
 	if (!gameState.isGameFinished) {
 		// движения
 		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
-			gameState.playerDirection = PlayerDirection::Right;
+			gameState.player.direction = PlayerDirection::Right;
 		}
 		else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) {
-			gameState.playerDirection = PlayerDirection::Up;
+			gameState.player.direction = PlayerDirection::Up;
 		}
 		else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
-			gameState.playerDirection = PlayerDirection::Left;
+			gameState.player.direction = PlayerDirection::Left;
 		}
 		else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) {
-			gameState.playerDirection = PlayerDirection::Down;
+			gameState.player.direction = PlayerDirection::Down;
 		}
 
 
 		// изменение состояния
-		switch (gameState.playerDirection) {
+		switch (gameState.player.direction) {
 		case PlayerDirection::Right: {
-			gameState.playerPosition.x += gameState.playerSpeed * deltaTime;
+			gameState.player.position.x += gameState.player.speed * deltaTime;
 			break;
 		}
 		case PlayerDirection::Up: {
-			gameState.playerPosition.y -= gameState.playerSpeed * deltaTime;
+			gameState.player.position.y -= gameState.player.speed * deltaTime;
 			break;
 		}
 		case PlayerDirection::Left: {
-			gameState.playerPosition.x -= gameState.playerSpeed * deltaTime;
+			gameState.player.position.x -= gameState.player.speed * deltaTime;
 			break;
 		}
 		case PlayerDirection::Down: {
-			gameState.playerPosition.y += gameState.playerSpeed * deltaTime;
+			gameState.player.position.y += gameState.player.speed * deltaTime;
 			break;
 		}
 		}
 
 
 		for (int i = 0; i < NUM_APPLES; ++i) {			
-			if (IsCircleCollide(gameState.playerPosition, PLAYER_SIZE / 2.f,
-				gameState.applesPositions[i], APPLE_SIZE / 2.f)) {
-				gameState.applesPositions[i] = GetRandomPositionInScreen();
+			if (IsCircleCollide(gameState.player.position, PLAYER_SIZE / 2.f,
+				gameState.apples[i].position, APPLE_SIZE / 2.f)) {
+				gameState.apples[i].position = GetRandomPositionInScreen();
 				++gameState.numEatenApples;
-				gameState.playerSpeed += ACCELERATION;
+				gameState.player.speed += ACCELERATION;
 			}
 
 		}
 
 		for (int i = 0; i < NUM_STONES; ++i) {
-			if (IsRectangleCollide(gameState.playerPosition, { PLAYER_SIZE ,PLAYER_SIZE }, 
-				gameState.stonePositions[i], {STONE_SIZE, STONE_SIZE })) {
+			if (IsRectangleCollide(gameState.player.position, { PLAYER_SIZE ,PLAYER_SIZE }, 
+				gameState.stones[i].position, {STONE_SIZE, STONE_SIZE })) {
 				gameState.isGameFinished = true;
 				gameState.timeSinceGameFinish = 0.f;
 			}
 		}
 
 		//об стенку
-		if (gameState.playerPosition.x - PLAYER_SIZE / 2.f < 0.f || 
-			gameState.playerPosition.x + PLAYER_SIZE / 2.f > SCREEN_WIDTH ||
-			gameState.playerPosition.y - PLAYER_SIZE / 2.f < 0.f || 
-			gameState.playerPosition.y + PLAYER_SIZE / 2.f > SCREEN_HEIGHT) {
+		if (gameState.player.position.x - PLAYER_SIZE / 2.f < 0.f ||
+			gameState.player.position.x + PLAYER_SIZE / 2.f > SCREEN_WIDTH ||
+			gameState.player.position.y - PLAYER_SIZE / 2.f < 0.f ||
+			gameState.player.position.y + PLAYER_SIZE / 2.f > SCREEN_HEIGHT) {
 
 			gameState.isGameFinished = true;
 			gameState.timeSinceGameFinish = 0.f;
@@ -194,38 +215,22 @@ void UpdateGame(GameState& gameState, float deltaTime) {
 		}
 		else {
 			gameState.background.setFillColor(sf::Color::Black);
-			//перезапуск игрока
-			gameState.playerPosition.x = SCREEN_WIDTH / 2.f;
-			gameState.playerPosition.y = SCREEN_HEIGHT / 2.f;
-			gameState.playerSpeed = INITIAL_SPEED;
-			gameState.playerDirection = PlayerDirection::Right;
-			//перезапуск яблок
-			for (int i = 0; i < NUM_APPLES; ++i) {
-				gameState.applesPositions[i] = GetRandomPositionInScreen();
-			}
-			//перезапуск камней
-			for (int i = 0; i < NUM_STONES; ++i) {
-				gameState.stonePositions[i] = GetRandomPositionInScreen();
-			}
-			//перезапуск состояния игры
-			int numEatenApples = 0;
-			gameState.isGameFinished = false;
-			gameState.timeSinceGameFinish = 0.f;
+			RestartGame(gameState);
 		}
 	}
 }
 
 void DrawGame(GameState& gameState, sf::RenderWindow& window) {
 	window.draw(gameState.background);
-	gameState.playerShape.setPosition(gameState.playerPosition.x, gameState.playerPosition.y);
-	window.draw(gameState.playerShape);
+	gameState.player.shape.setPosition(gameState.player.position.x, gameState.player.position.y);
+	window.draw(gameState.player.shape);
 	for (int i = 0; i < NUM_APPLES; ++i) {
-		gameState.applesShape[i].setPosition(gameState.applesPositions[i].x, gameState.applesPositions[i].y);
-		window.draw(gameState.applesShape[i]);
+		gameState.apples[i].shape.setPosition(gameState.apples[i].position.x, gameState.apples[i].position.y);
+		window.draw(gameState.apples[i].shape);
 	}
 	for (int i = 0; i < NUM_STONES; ++i) {
-		gameState.stoneShape[i].setPosition(gameState.stonePositions[i].x, gameState.stonePositions[i].y);
-		window.draw(gameState.stoneShape[i]);
+		gameState.stones[i].shape.setPosition(gameState.stones[i].position.x, gameState.stones[i].position.y);
+		window.draw(gameState.stones[i].shape);
 	}
 }
 
